ffe1b6c389c5039d477747f61ed665878fce3e6e
jvcoutinho <joao.coutinho9@gmail.com>
2019-10-08 18:31:10 -0300

    Renaming

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,16 +1,16 @@
 package io.github.joaomlneto.travis_ci_tutorial_java;
 
 public class SimpleCalculator {
-	public int add(int a, int b) {
+	public int addIntegers(int a, int b) {
 		return a + b;
 	}
-	public int sub(int a, int b) {
+	public int subIntegers(int a, int b) {
 		return a - b;
 	}
-	public int mul(int a, int b) {
+	public int mulIntegers(int a, int b) {
 		return a * b;
 	}
-	public int div(int a, int b) {
+	public int divIntegers(int a, int b) {
 		return a / b;
 	}
 	public static final void main(String[] args) {
