d7ee92c36f6a654c2cd5f4a00f3fdbda5a5c5e6d
jvcoutinho <joao.coutinho9@gmail.com>
2019-10-16 00:05:54 -0300

    Trigger build

--- src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/io/github/joaomlneto/travis_ci_tutorial_java/SimpleCalculator.java
@@ -1,21 +1,27 @@
 package io.github.joaomlneto.travis_ci_tutorial_java;
 
 public class SimpleCalculator {
+<<<<<<< MINE
+	public int mul2(int a, int b) {
+		return a * b;
+	}
+======= 
+	public int mulIntegers(int a, int b) {
+		return a * b;
+	}
+>>>>>>> YOURS
+
+	public void hello() {
+
+	}
 	public int addIntegers(int a, int b) {
 		return a + b;
 	}
 	public int subIntegers(int a, int b) {
 		return a - b;
 	}
-	public int mulIntegers(int a, int b) {
-		return a * b;
-	}
 	public int divIntegers(int a, int b) {
 		return a / b;
-	}
-
-	public void hello() {
-
 	}
 	public static final void main(String[] args) {
 		System.out.println("Hello world?");
